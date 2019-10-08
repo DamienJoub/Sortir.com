2ab1ed974cedc15d70574e62fb3f726160432d09
mcataldo2018 <mickael.cataldo2018@campus-eni.fr>
2019-10-08 14:58:57 +0200

    gestion mon compte

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -8,6 +8,7 @@ use App\Entity\Participant;
 use App\Form\GestionProfilType;
 use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParticipantController extends Controller
@@ -16,7 +17,7 @@ class ParticipantController extends Controller
     /**
      * @Route("/monProfil", name ="monProfil")
      */
-    public function gestionParticipant(){
+    public function gestionParticipant(Request $request){
 
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository(Participant::class);
@@ -24,10 +25,12 @@ class ParticipantController extends Controller
         $participant = new Participant();
 
         $participant = $repo->findByIdentifiant('mm.cc@jojo.fr');
-
-
-
         $participantForm = $this->createForm(GestionProfilType::class, $participant);
+        $participantForm->handleRequest($request);
+
+        if($participantForm->isSubmitted() && $participantForm->isValid()){
+            return $this->redirectToRoute("main_home");
+        }
 
 
         return $this->render("main/monProfil.html.twig",[
