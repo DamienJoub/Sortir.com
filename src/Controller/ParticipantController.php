f1d2f51ddd8975828f7dd7edd065013276111065
jhartzer2018 <julien.hartzer@hotmail.fr>
2019-10-08 16:49:56 +0200

    Attention à la mousse

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
