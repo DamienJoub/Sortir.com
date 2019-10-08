d82eb66d809d3befc61acab8ab4b8e72626c089c
mcataldo2018 <mickael.cataldo2018@campus-eni.fr>
2019-10-08 12:27:10 +0200

    participants

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,7 +4,24 @@
 namespace App\Controller;
 
 
-class ParticipantController
+use App\Entity\Participant;
+use App\Form\GestionProfilType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends Controller
 {
 
+    /**
+     * @Route("/monProfil", name ="monProfil")
+     */
+    public function gestionParticipant(){
+        $participant = new Participant();
+        $participantForm = $this->createForm(Participant::class, $participant);
+
+
+        return $this->render("main/monProfil.html.twig",[
+            "participantForm" => $participantForm->createView()
+        ]);
+    }
 }
