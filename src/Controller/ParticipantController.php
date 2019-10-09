6cfcb705aa0fb3b6f16e6027c22f8e959ce83f2d
mcataldo2018 <mickael.cataldo2018@campus-eni.fr>
2019-10-09 14:15:40 +0200

    gestion mon compte

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -29,42 +29,58 @@ class ParticipantController extends Controller
      */
     public function gestionParticipant(Request $request){
 
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository(Participant::class);
+        $session = $request->getSession();
 
-        $participant = new Participant();
-        $participant = $repo->findByIdentifiant('mm.cc@jojo.fr');
-        $participantForm = $this->createForm(GestionProfilType::class, $participant,["participant" => $participant]);
+        //*********************
+        //Bouchon
+        $session->set('identifiant', 'mm.cc@jojo.fr');
+        //fin du bouchon
+        //*********************
+
+        //Si l'identifiant de l'utilisatuer est renseigné
+        if($session->has('identifiant')){
+
+            $em = $this->getDoctrine()->getManager();
+            $repo = $em->getRepository(Participant::class);
+
+            $participant = new Participant();
+            $participant = $repo->findByIdentifiant($session->get('identifiant'));
+            $participantForm = $this->createForm(GestionProfilType::class, $participant,["participant" => $participant]);
 
 
-        $participantForm->handleRequest($request);
+            $participantForm->handleRequest($request);
 
-        if($participantForm->isSubmitted() && $participantForm->isValid()){
-            // si le mot de passe est saisi on le modifie en base
-            if($participantForm->get('mot_de_passe')->getData() != null &&
-                $participantForm->get('confirmation')->getData()  != null &&
-                $participantForm->get('mot_de_passe')->getData() == $participantForm->get('confirmation')->getData()){
-                $participant->setMotDePasse($participantForm->get('mot_de_passe')->getData());
+            if($participantForm->isSubmitted() && $participantForm->isValid()){
+                // si le mot de passe est saisi on le modifie en base
+                if($participantForm->get('mot_de_passe')->getData() != null &&
+                    $participantForm->get('confirmation')->getData()  != null &&
+                    $participantForm->get('mot_de_passe')->getData() == $participantForm->get('confirmation')->getData()){
+                    $participant->setMotDePasse($participantForm->get('mot_de_passe')->getData());
+                }
+
+                $participant->setNom($participantForm->get('nom')->getData());
+                $participant->setPrenom($participantForm->get('prenom')->getData());
+                $participant->setTelephone($participantForm->get('telephone')->getData());
+                $participant->setCampus($participantForm->get('campus')->getData());
+                $participant->setMail($participantForm->get('mail')->getData());
+                $em->persist($participant);
+
+                try{
+                    $em->flush();
+                }catch (UniqueConstraintViolationException $e){
+                    return $this->redirectToRoute("monProfil");
+                }
+
+                return $this->redirectToRoute("main_home");
             }
 
-            $participant->setNom($participantForm->get('nom')->getData());
-            $participant->setPrenom($participantForm->get('prenom')->getData());
-            $participant->setTelephone($participantForm->get('telephone')->getData());
-            $participant->setCampus($participantForm->get('campus')->getData());
-            $participant->setMail($participantForm->get('mail')->getData());
-            $em->persist($participant);
+            return $this->render("main/monProfil.html.twig",[
+                "participantForm" => $participantForm->createView()
+            ]);
 
-            try{
-                $em->flush();
-            }catch (UniqueConstraintViolationException $e){
-                return $this->redirectToRoute("monProfil");
-            }
-
+         //sinon on retourn à l'aceuil
+        }else{
             return $this->redirectToRoute("main_home");
         }
-
-        return $this->render("main/monProfil.html.twig",[
-            "participantForm" => $participantForm->createView()
-        ]);
     }
 }
